8052a73702c61953e71a38a1fd3c926f9d7a6bc8
md enamul <hfiroz1998@gmail.com>
2024-06-25 23:28:59 +0600

    new 2

--- index.js
+++ index.js
@@ -77,10 +77,8 @@ console.log(sortArray(arrayShort))
 // 8 Ans 
 let arrayCapital = "hello"; 
 function capitalizeFirstLetter(str){
-    str = str.toLowerCase().split(' ');
-	for (var i = 0; i < str.length; i++) {
-		str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
-	}
-	return str.join(' ');
+    return str[0].toUpperCase() + str.slice(1);
+
 }
 console.log(capitalizeFirstLetter(arrayCapital))
+
